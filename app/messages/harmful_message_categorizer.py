d2f5eddce5b441ff757cf82dbc83a6a2aa15848c
GintasS <svedgintas@gmail.com>
2024-09-10 16:58:17 -0700

    Wrappers for BAML generated python code.

---
+++ app/messages/harmful_message_categorizer.py
@@ -0,0 +1,14 @@
+from baml_client import b
+from baml_client.types import HarmfulMessageCategory
+
+import logging
+
+async def categorize_harmful_message(message : str) -> HarmfulMessageCategory:
+  try:
+    category = b.ExtractMessageCategoryFromMessage(message)
+  except Exception as exc:
+    logging.info("[Message Categorizer API] Error on categorizing a harmful message.", exc_info=True)
+    return HarmfulMessageCategory.Other
+  return category
+  
+  
---
+++ app/messages/is_message_a_harmful_message_categorizer.py
@@ -0,0 +1,12 @@
+from baml_client import b
+
+import logging
+
+async def is_discord_message_a_harmful_message(message : str) -> bool:
+  try:
+    message_type = b.IdentifyHarmfulDiscordMessage(message)
+    return message_type.is_harmful is True
+  except Exception as exc:
+    logging.info("[Message Categorizer API] Error on identifying whether a message is a harmful one", exc_info=True)
+    return False
+#logging.info("The error indicates that the function IdentifyHarmfulDiscordMessage is not an asynchronous function and thus cannot be awaited. We need to call it without 'await' and handle it accordingly.")
