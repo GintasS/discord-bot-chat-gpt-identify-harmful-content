8f60318da2471a7fbc0b1541bd0ca6b5a936173a
GintasS <svedgintas@gmail.com>
2024-09-10 19:04:14 -0700

    Some small code style improvements.

--- app/messages/harmful_message_categorizer.py
+++ app/messages/harmful_message_categorizer.py
@@ -6,7 +6,7 @@ import logging
 async def categorize_harmful_message(message : str) -> HarmfulMessageCategory:
   try:
     category = b.ExtractMessageCategoryFromMessage(message)
-  except Exception as exc:
+  except Exception as ex:
     logging.info("[Message Categorizer API] Error on categorizing a harmful message.", exc_info=True)
     return HarmfulMessageCategory.Other
   return category
--- app/messages/is_message_a_harmful_message_categorizer.py
+++ app/messages/is_message_a_harmful_message_categorizer.py
@@ -6,7 +6,7 @@ async def is_discord_message_a_harmful_message(message : str) -> bool:
   try:
     message_type = b.IdentifyHarmfulDiscordMessage(message)
     return message_type.is_harmful is True
-  except Exception as exc:
+  except Exception as ex:
     logging.info("[Message Categorizer API] Error on identifying whether a message is a harmful one", exc_info=True)
     return False
 #logging.info("The error indicates that the function IdentifyHarmfulDiscordMessage is not an asynchronous function and thus cannot be awaited. We need to call it without 'await' and handle it accordingly.")
